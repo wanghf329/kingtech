7995ed6e96463f94ec6fa2599d5d0f95b46366db
wanghf329 <wanghf329@163.com>
2017-11-28 13:02:39 +0800

    配置

--- kingtech-parent/kingtech-web/src/main/java/com/kingtech/web/controller/HomeApiController.java
+++ kingtech-parent/kingtech-web/src/main/java/com/kingtech/web/controller/HomeApiController.java
@@ -8,7 +8,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import com.kingtech.common.config.BaseConfig;
 import com.kingtech.web.commons.base.service.FinanceService;
@@ -61,6 +60,12 @@ public class HomeApiController {
 		
 		return jsonObject.toJSONString();
 	}
+	
+	@RequestMapping(method = RequestMethod.GET,value="/logout")
+	public String logout(HttpSession session,Model model) {
+		session.invalidate();
+		return "/login";
+	} 
 		
 		
 		
