b7c10f20831bd31ff0e98eeb01cd73b248f5e435
baijt <317347607@qq.com>
2017-11-28 12:59:28 +0800

    登录人

--- kingtech-parent/kingtech-web/src/main/java/com/kingtech/web/controller/HomeApiController.java
+++ kingtech-parent/kingtech-web/src/main/java/com/kingtech/web/controller/HomeApiController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
@@ -56,10 +57,9 @@ public class HomeApiController {
 		}else  {
 			jsonObject.put("result", "failed");
 			jsonObject.put("resultDesc", "登陆名或密码错误");
-
+			return "/login";
 		}
 		
-		return jsonObject.toJSONString();
 	}
 		
 		
