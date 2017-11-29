3fc4ee3e6d5f67d99463ae3d45a8d9e546d948c6
baijt <317347607@qq.com>
2017-11-29 20:30:43 +0800

    model

--- kingtech-parent/kingtech-dao/src/main/java/com/kingtech/dao/rdbms/CollateralDAO.java
+++ kingtech-parent/kingtech-dao/src/main/java/com/kingtech/dao/rdbms/CollateralDAO.java
@@ -1,9 +1,17 @@
 package com.kingtech.dao.rdbms;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 
 import com.kingtech.dao.entity.Collateral;
 
 public interface CollateralDAO extends PagingAndSortingRepository<Collateral, String> {
+	
+	
+	@Query("select c from Collateral c where c.loanContractId = :loanContractId")
+	public List<Collateral> listByloanContractId(@Param("loanContractId")String loanContractId);
 	
 }
--- kingtech-parent/kingtech-dao/src/main/java/com/kingtech/dao/rdbms/GuaranteeDAO.java
+++ kingtech-parent/kingtech-dao/src/main/java/com/kingtech/dao/rdbms/GuaranteeDAO.java
@@ -1,9 +1,17 @@
 package com.kingtech.dao.rdbms;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 
 import com.kingtech.dao.entity.Guarantee;
 
 public interface GuaranteeDAO extends PagingAndSortingRepository<Guarantee, String> {
+	
+	
+	@Query("select g from Guarantee g where g.loanContractId = :loanContractId")
+	public List<Guarantee> listByloanContractId(@Param("loanContractId")String loanContractId);
 	
 }
