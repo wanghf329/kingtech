b8f03d3417ee97b8a5a5bf0b302aaaa15963f2b7
baijt <317347607@qq.com>
2017-11-29 19:25:40 +0800

    model

--- kingtech-parent/kingtech-dao/src/main/java/com/kingtech/dao/rdbms/ContractDAO.java
+++ kingtech-parent/kingtech-dao/src/main/java/com/kingtech/dao/rdbms/ContractDAO.java
@@ -1,9 +1,15 @@
 package com.kingtech.dao.rdbms;
 
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 
 import com.kingtech.dao.entity.Contract;
+import com.kingtech.enums.PushStatus;
 
 public interface ContractDAO extends PagingAndSortingRepository<Contract, String> {
-	
+	@Modifying
+	@Query("update Contract c set c.pushStatus = :pushStatus  where c.reqId = :reqId")
+	public int updateStatusByReqId(@Param("reqId")String reqId,@Param("pushStatus")PushStatus pushStatus);
 }
