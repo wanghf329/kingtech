34f8ee804d79bb19e6fd12c1e6f0cd1f2a043ef6
Tson415 <tangguotao415@163.com>
2017-11-29 20:03:40 +0800

    提价

--- kingtech-parent/kingtech-dao/src/main/java/com/kingtech/dao/entity/EnterpriseCustomer.java
+++ kingtech-parent/kingtech-dao/src/main/java/com/kingtech/dao/entity/EnterpriseCustomer.java
@@ -24,66 +24,66 @@ import com.kingtech.enums.ScaleType;
 public class EnterpriseCustomer extends PackageEntity{
 	
 	//企业名称
-	@Column(name="CORPORATE_NAME")
+	@Column(name="CORPORATE_NAME",nullable = false)
 	private String  corporateName;
 	
 	//企业规模：1) 个体工商户  2) 农村专业合作组织 3) 微型企业4)小型企业5)中型企业6)大型企业7)其他组织
 	@Enumerated(EnumType.STRING)
-	@Column(name="SCALE")
+	@Column(name="SCALE",nullable = false)
 	private ScaleType  scale;
 	
 	//产业类型：1) 第一产业   2）第二产业  3）第三产业
 	@Enumerated(EnumType.STRING)
-	@Column(name="INDUSTRY_TYPE")
+	@Column(name="INDUSTRY_TYPE",nullable = false)
 	private  IndustryType industryType;
 	
 	//所属行业：1）居民服务和其他服务业,2）建筑业, 3）交通运输、仓储和邮政业, 4）农、林、牧、渔业, 5）采矿业, 6）制造业, 7）电力、燃气及水的生产和供应业, 8）信息传输、计算机服务和软件业, 9）批发和零售业,10）住宿和餐饮业, 11）房地产业, 12）租赁和商务服务业, 13）其他
 	@Enumerated(EnumType.STRING)
-	@Column(name="INDUSTRY_INVOLVED")
+	@Column(name="INDUSTRY_INVOLVED",nullable = false)
 	private IndustryEnum  industryinvolved;
 		
 	//组织机构代码
-	@Column(name="ORGANIZATION_CODE",nullable = false)
+	@Column(name="ORGANIZATION_CODE")
 	private String  organizationcode;
 		
 	//公司注册登记号
-	@Column(name="REG_CODE",nullable = false)
+	@Column(name="REG_CODE")
 	private String  regcode;
 		
 	//工商注册登记机关
-	@Column(name="REG_OFFICE",nullable = false)
+	@Column(name="REG_OFFICE")
 	private String  regoffice;
 		
 	//工商注册登记日期，格式YYYY-MM-DD
-	@Column(name="REG_DATE",nullable = false)
+	@Column(name="REG_DATE")
 	private Date  regDate;
 		
 	//国税税务登记号
-	@Column(name="NATIONAL_REG_NUM",nullable = false)
+	@Column(name="NATIONAL_REG_NUM")
 	private String  nationalregNum;
 		
 	//地税税务登记号
-	@Column(name="LAND_REG_NUM",nullable = false)
+	@Column(name="LAND_REG_NUM")
 	private String  landregNum;
 		
 	//营业执照号
-	@Column(name="LICENCE")
+	@Column(name="LICENCE",nullable = false)
 	private String  licence;
 		
 	//营业执照截止日期，格式YYYY-MM-DD
-	@Column(name="LICENCE_END_DATE")
+	@Column(name="LICENCE_END_DATE",nullable = false)
 	private String  licenceEndDate;
 		
 	//企业性质
-	@Column(name="NATURE",nullable = false)
+	@Column(name="NATURE")
 	private String  nature;
 		
 	//从业人数
-	@Column(name="EMPLOY_NUM",nullable = false)
+	@Column(name="EMPLOY_NUM")
 	private int  employNum;
 		
 	//法定代表人
-	@Column(name="LEGAL_REPRESENTATIVE",nullable = false)
+	@Column(name="LEGAL_REPRESENTATIVE")
 	private String  legalRepresentative;
 		
 	//成立日期，格式YYYY-MM-DD
@@ -91,63 +91,63 @@ public class EnterpriseCustomer extends PackageEntity{
 	private Date  bulidDate;
 		
 	//实际控制人
-	@Column(name="ACTUAL_CONTROLLER",nullable = false)
+	@Column(name="ACTUAL_CONTROLLER")
 	private String  actualController;
 		
 	//注册资本
-	@Column(name="REG_CAPITAL",nullable = false)
+	@Column(name="REG_CAPITAL")
 	private BigDecimal  regCapital;
 		
 	//实收资本
-	@Column(name="REALLY_CAPITAL",nullable = false)
+	@Column(name="REALLY_CAPITAL")
 	private BigDecimal  reallyCapital;
 		
 	//经营范围
-	@Column(name="BUSINESS_SCOPE",nullable = false)
+	@Column(name="BUSINESS_SCOPE")
 	private String  businessScope;
 		
 	//注册地址
-	@Column(name="REG_ADDRESS",nullable = false)
+	@Column(name="REG_ADDRESS")
 	private String  regAddress;
 		
 	//通讯地址（省份）
-	@Column(name="CONTACT_ADDRESS_PROVINCE")
+	@Column(name="CONTACT_ADDRESS_PROVINCE",nullable = false)
 	private String  contactAddressProvince;
 		
 	//通讯地址（市）
-	@Column(name="CONTACT_ADDRESS_CITY")
+	@Column(name="CONTACT_ADDRESS_CITY",nullable = false)
 	private String  contactAddresscity;
 		
 	//通讯地址（区/县）
-	@Column(name="CONTACT_ADDRESS_DISTRICT")
+	@Column(name="CONTACT_ADDRESS_DISTRICT",nullable = false)
 	private String  contactAddressDistrict;
 		
 	//通讯地址（详细）
-	@Column(name="CONTACT_ADDRESS",nullable = false)
+	@Column(name="CONTACT_ADDRESS")
 	private String  contactAddress;
 		
 	//邮政编码
-	@Column(name="POST_CODE",nullable = false)
+	@Column(name="POST_CODE")
 	private String  postcode;
 		
 	//联系电话
-	@Column(name="PHONE",nullable = false)
+	@Column(name="PHONE")
 	private String  phone;
 		
 	//联系人
-	@Column(name="LINKMAN",nullable = false)
+	@Column(name="LINKMAN")
 	private String  linkman;
 		
 	//传真
-	@Column(name="FAX",nullable = false)
+	@Column(name="FAX")
 	private String  fax;
 		
 	//公司邮箱
-	@Column(name="EMAIL",nullable = false)
+	@Column(name="EMAIL")
 	private String  email;
 		
 	//网址
-	@Column(name="WEB_SITE",nullable = false)
+	@Column(name="WEB_SITE")
 	private String  webSite;
 
 	public EnterpriseCustomer(String loanContractId, String corporateName,
---
+++ kingtech-parent/kingtech-dao/src/main/java/com/kingtech/dao/entity/PersonalCustomer.java
@@ -0,0 +1,134 @@
+package com.kingtech.dao.entity;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.Table;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import com.kingtech.dao.entity.base.PackageEntity;
+import com.kingtech.enums.CertType;
+import com.kingtech.enums.SexEnum;
+import com.kingtech.enums.YesNoEnum;
+
+
+@Data
+@Entity
+@Table(name="TB_ENTERPRISE_CUSTOMER")
+@NoArgsConstructor
+public class PersonalCustomer extends PackageEntity{
+	//姓名
+	@Column(name="NAME",nullable = false)
+	private String name;
+
+	//性别：1）男 2）女
+	@Enumerated(EnumType.ORDINAL)
+	@Column(name="SEX",nullable = false)
+	private SexEnum sex;
+	
+	//证件类型：1）居民身份证 2）护照,  3）驾驶证
+	@Enumerated(EnumType.STRING)
+	@Column(name="CATEGORY",nullable = false)
+	private CertType category;
+	
+	// 证件号码
+	@Column(name="CARD_NUM",nullable = false)
+	private String cardNum;
+	
+	// 联系电话
+	@Column(name="PHONE",nullable = false)
+	private String phone;
+	
+	//是否农牧民：1）是   2）否
+	@Enumerated(EnumType.STRING)
+	@Column(name="FARMERS_FLAG",nullable = false)
+	private YesNoEnum farmersFlag;
+	
+	//学历
+	@Column(name="EDUCATION")
+	private String education;
+	
+	// 传真
+	@Column(name="FAX")
+	private String fax;
+	
+	// 电子邮件
+	@Column(name="EMAIL")
+	private String email;
+	
+	//婚姻状况
+	@Column(name="MARRIAGE")
+	private String marriage;
+	
+	//国籍
+	@Column(name="NATIONALITY")
+	private String nationality;
+	
+	//出生日期，格式YYYY-MM-DD
+	@Column(name="BIRTH_DATE")
+	private Date birthDate;
+	
+	//民族
+	@Column(name="NATION")
+	private String nation;
+	
+	//固定住址（省份）
+	@Column(name="ADDRESS_PROVINCE",nullable = false)
+	private String addressProvince;
+	
+	//固定住址（市）
+	@Column(name="ADDRESS_CITY",nullable = false)
+	private String addressCity;
+	
+	// 固定住址（区/县）
+	@Column(name="ADDRESS_DISTRICT",nullable = false)
+	private String addressDistrict;
+	
+	// 固定住址（详细）
+	@Column(name="ADDRESS")
+	private String address;
+	
+	//邮政编码
+	@Column(name="POST_CODE")
+	private String postCode;
+	
+	// 户口所在地
+	@Column(name="RESIDENCE")
+	private String residence;
+	
+	// 籍贯
+	@Column(name="NATIVE_PLACE")
+	private String nativePlace;
+	
+	// 现工作单位
+	@Column(name="WORK_UNIT")
+	private String workUnit;
+	
+	//职务
+	@Column(name="POST")
+	private String post;
+
+	public PersonalCustomer(String loanContractId, String name, SexEnum sex,
+			CertType category, String cardNum, String phone,
+			YesNoEnum farmersFlag, String addressProvince, String addressCity,
+			String addressDistrict) {
+		super(loanContractId);
+		this.name = name;
+		this.sex = sex;
+		this.category = category;
+		this.cardNum = cardNum;
+		this.phone = phone;
+		this.farmersFlag = farmersFlag;
+		this.addressProvince = addressProvince;
+		this.addressCity = addressCity;
+		this.addressDistrict = addressDistrict;
+	}
+
+	
+	
+}
