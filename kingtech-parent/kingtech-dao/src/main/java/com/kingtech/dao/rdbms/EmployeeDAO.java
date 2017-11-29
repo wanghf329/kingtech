9c03e1eacc071838e1177e830914675b2d92a112
wanghf329 <wanghf329@163.com>
2017-11-29 19:22:41 +0800

    daima

---
+++ kingtech-parent/kingtech-dao/src/main/java/com/kingtech/dao/rdbms/EmployeeDAO.java
@@ -0,0 +1,18 @@
+package com.kingtech.dao.rdbms;
+
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+
+import com.kingtech.dao.entity.Contract;
+import com.kingtech.enums.PushStatus;
+
+public interface EmployeeDAO extends PagingAndSortingRepository<Contract, String> {
+	
+	
+	@Modifying
+	@Query("update Employee e set e.pushStatus = :pushStatus  where e.reqId = :reqId")
+	public int updateStatusByReqId(@Param("reqId")String reqId,@Param("pushStatus")PushStatus pushStatus);
+	
+}
