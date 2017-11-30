cf8018ca6045bf3d41eeda4eb9f4735549af3dc3
wangyu <jaywangyu@163.com>
2017-11-30 10:50:18 +0800

    commit

--- kingtech-parent/kingtech-common/src/main/java/com/kingtech/enums/PledgeTypeEnum.java
+++ kingtech-parent/kingtech-common/src/main/java/com/kingtech/enums/PledgeTypeEnum.java
@@ -1,41 +1,17 @@
 package com.kingtech.enums;
 
 public enum PledgeTypeEnum implements BaseEnum {
-	MORTGAGE("1"),//抵押
-	PLEDGE("2");//质押
+	S_1("抵押"), 
+	S_2("质押");
 
 	private String key;
-	
+
 	private PledgeTypeEnum(String key) {
 		this.key = key;
 	}
-	
+
 	@Override
 	public String getKey() {
 		return key;
-	}
-
-	public static PledgeTypeEnum getValue(String key){
-		switch (key) {
-		case "1":
-			return PledgeTypeEnum.MORTGAGE;
-		case "2":
-			return PledgeTypeEnum.PLEDGE;
-		default:
-			break;
-		}
-		return null;
-	}
-	
-	public String getTips(){
-		switch (key) {
-			case "1":
-				return "抵押";
-			case "2":
-				return "质押";
-			default:
-				break;
-		}
-		return null;
 	}
 }
