b08355ec144cb334e2a4f5ef366bfc8cb5c1ec02
wanghf329 <wanghf329@163.com>
2017-12-05 14:41:23 +0800

    合同修改页面

--- kingtech-parent/kingtech-dao/src/main/java/com/kingtech/dao/rdbms/RepayPlanDAO.java
+++ kingtech-parent/kingtech-dao/src/main/java/com/kingtech/dao/rdbms/RepayPlanDAO.java
@@ -11,7 +11,7 @@ import com.kingtech.dao.entity.RepayPlan;
 
 public interface RepayPlanDAO extends PagingAndSortingRepository<RepayPlan, String> {
 	
-	@Query("select r from RepayPlan r where r.loanContractId = :loanContractId")
+	@Query("select r from RepayPlan r where r.loanContractId = :loanContractId order by r.repayDate ")
 	public List<RepayPlan> listByloanContractId(@Param("loanContractId")String loanContractId);
 	
 	@Modifying
