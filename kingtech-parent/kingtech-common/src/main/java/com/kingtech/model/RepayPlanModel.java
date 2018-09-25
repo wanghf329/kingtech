5b481c8b40ff84d77437f8d27ed86d5366bac2cf
toodancing <wangxiaoe@coamc.com>
2018-09-25 14:49:02 +0800

    冲突

--- kingtech-parent/kingtech-common/src/main/java/com/kingtech/model/RepayPlanModel.java
+++ kingtech-parent/kingtech-common/src/main/java/com/kingtech/model/RepayPlanModel.java
@@ -1,18 +1,10 @@
 package com.kingtech.model;
 
 import java.math.BigDecimal;
-import java.util.Date;
 
-import javax.persistence.Column;
-
-import com.alibaba.fastjson.annotation.JSONField;
-
->>>>>>> 75f1a3fc34ca31dbb628c4fd2925c0db00b56d40
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import com.alibaba.fastjson.annotation.JSONField;
 
 
 @Data
