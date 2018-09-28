0fc9eed7abf4d97a0ac87e253fcba5559cac7324
wangyu <jaywangyu@163.com>
2018-09-28 11:01:06 +0800

    modify

--- kingtech-parent/kingtech-dao/src/main/java/com/kingtech/dao/rdbms/ProvisionInfoDAO.java
+++ kingtech-parent/kingtech-dao/src/main/java/com/kingtech/dao/rdbms/ProvisionInfoDAO.java
@@ -1,5 +1,7 @@
 package com.kingtech.dao.rdbms;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
@@ -13,4 +15,7 @@ public interface ProvisionInfoDAO extends PagingAndSortingRepository<ProvisionIn
 	@Modifying
 	@Query("update ProvisionInfo c set c.pushStatus = :pushStatus  where c.reqId = :reqId")
 	public int updateStatusByReqId(@Param("reqId")String reqId,@Param("pushStatus")PushStatus pushStatus);
+	
+	@Query("select c from ProvisionInfo c where c.pushStatus in :pushStatus")
+	public List<ProvisionInfo> listBypushStatus(@Param("pushStatus") List<PushStatus> pushStatus);
 }
